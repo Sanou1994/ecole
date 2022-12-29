e718b66dc2df515ef6e1132ceed7f0a8f79b1499
aboubadra <moumounid44@gmail.com>
2022-12-29 12:01:31 +0000

    ajout des getters et setters

--- src/main/java/com/gestion_ecole/ecole/entities/MailSend.java
+++ src/main/java/com/gestion_ecole/ecole/entities/MailSend.java
@@ -4,11 +4,58 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @AllArgsConstructor @NoArgsConstructor
+
 public class MailSend {
 	private String email;
 	private String name;
     private String subject;
     private String message;
+    
+	public MailSend() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	public MailSend(String email, String name, String subject, String message) {
+		super();
+		this.email = email;
+		this.name = name;
+		this.subject = subject;
+		this.message = message;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+    
+    
 }
--- src/main/java/com/gestion_ecole/ecole/entities/Paiement.java
+++ src/main/java/com/gestion_ecole/ecole/entities/Paiement.java
@@ -8,7 +8,7 @@ import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
-@Entity @Data
+@Entity 
 public class Paiement {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -21,4 +21,88 @@ public class Paiement {
 	private String status;
 	@ManyToOne
 	private Departement departement;
+	
+	public Paiement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Paiement(Long id, User user, long datePaiement, String order_number, double montant, String typeTransaction,
+			String status, Departement departement) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.datePaiement = datePaiement;
+		this.order_number = order_number;
+		this.montant = montant;
+		this.typeTransaction = typeTransaction;
+		this.status = status;
+		this.departement = departement;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public long getDatePaiement() {
+		return datePaiement;
+	}
+
+	public void setDatePaiement(long datePaiement) {
+		this.datePaiement = datePaiement;
+	}
+
+	public String getOrder_number() {
+		return order_number;
+	}
+
+	public void setOrder_number(String order_number) {
+		this.order_number = order_number;
+	}
+
+	public double getMontant() {
+		return montant;
+	}
+
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+
+	public String getTypeTransaction() {
+		return typeTransaction;
+	}
+
+	public void setTypeTransaction(String typeTransaction) {
+		this.typeTransaction = typeTransaction;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Departement getDepartement() {
+		return departement;
+	}
+
+	public void setDepartement(Departement departement) {
+		this.departement = departement;
+	}
+	
+	
 }
